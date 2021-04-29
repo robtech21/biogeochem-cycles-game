2f06756c6bb0c9ed11f0ffee72d536ae784747dd
Robert Furr <robert@megley.com>
2021-04-29 15:07:24 +0000

    Made base framework

---
+++ src/bin/__main__.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+#    My biogeochemical cycles game
+#    Copyright (C) 2021  Robert Furr
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+from os import system
+from termcolor import colored
+
+def clr():
+    '''Clear screen function'''
+    system('clear')
+
+# Shortcode functions
+pnt   = print
+inpt  = input
+color = colored
+green = 'green'
+red   = 'red'
+
+while True:
+  clr()
+  choice = inpt(color('''
+|==========================|
+|Biogeochemical Cycles Game|
+|==========================|
+|1. Start                  |
+|2. Credits                |
+|3. Exit                   |
+|==========================|
+> ''',green))
+  if choice == '1':
+    pnt('none')
+  if choice == '2':
+    inpt(color('''
+CREDITS:
+
+Developer:    Robert Furr - https://github.com/robtech21
+Modules used: termcolor   - https://pypi.org/project/termcolor/
+
+    License:
+
+    My biogeochemical cycles game
+    Copyright (C) 2021  Robert Furr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Press enter to go back to title
+''',green))
